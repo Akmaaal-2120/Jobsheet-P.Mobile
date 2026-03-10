5fb76915efb899835322c2c3f7675af05b5635a0
Akmal-001 <kitsunenpp@gmail.com>
2026-03-10 23:57:08 +0700

    mempelajari loop, validasi dan git

---
+++ Week#03/README.md
@@ -0,0 +1,28 @@
+if else dan switch case bisa digunakan untuk validasi 
+
+perulangan ada 4 yaitu for loop, for in, while, do while
+
+
+
+git clone = untuk cloning repo
+
+git push origin <nama branch> = untuk push ke branch yang diinginkan
+
+git pull origin <nama branch> = untuk pull ke branch yang diinginkan
+
+git add file.dart = untuk menambah file ke staged change
+
+git commit -m "pesan" = untuk commit
+
+git branch <nama branch> = untuk menambah branch
+
+git branch -d <nama branch> = hapus branch
+
+git checkout <nama branch> = pindah branch
+
+git checkout -b <nama branch> = buat dan pindah branch
+
+git merge <nama branch> = merge branch (harus pindah ke branch tujuan dulu)
+
+
+
---
+++ Week#03/src/bin/jadwal_mingguan.dart
@@ -0,0 +1,34 @@
+import '../lib/tambahJadwal.dart';
+import '../lib/lihatJadwal.dart';
+import '../lib/lihatJadwalHari.dart';
+import 'dart:io';
+void main(){
+  int menu; //variabel untuk input menu
+  do{
+    print("\n===== JADWAL BELAJAR MINGGUAN =====");
+    print("1. Tambah Jadwal");
+    print("2. Lihat Semua Jadwal");
+    print("3. Lihat Jadwal Berdasar Hari");
+    print("4. Keluar");
+
+    stdout.write("Pilih Menu: ");
+    menu = int.parse(stdin.readLineSync()!); // input angka untuk ganti menu
+
+    switch(menu){  // switch case untuk perpindahan menu
+      case 1:
+        tambahJadwal();
+        break;
+      case 2:
+        lihatJadwal();
+        break;
+      case 3:
+        lihatJadwalHari();
+        break;
+      case 4:
+        print("Keluar...");
+        break;
+      default:
+        print("Menunya salah!");
+    }
+  } while (menu != 4); // kondisi ini jika true maka akan loop ke menu utama
+}
